739391ecc39d0dd48f1d947c1f88c533f1e6adfc
Ivo Zivkov <ivozi@quickplay.com>
2017-05-23 14:24:38 -0400

    Update doc.

--- create-readme.sh
+++ create-readme.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-cat "chapters/title.md" "chapters/introduction.md" "chapters/nomenclature.md" "chapters/limitations.md" "chapters/environment.md" "chapters/creating-your-own-sheet-app.md" "chapters/application-structure.md" "chapters/the-google-spreadsheet.md" "chapters/settings.md" "chapters/future-work.md" > README.md
+cat "chapters/title.md" "chapters/introduction-dev.md" "chapters/nomenclature.md" "chapters/limitations.md" "chapters/environment.md" "chapters/creating-your-own-sheet-app.md" "chapters/application-structure.md" "chapters/the-google-spreadsheet.md" "chapters/settings.md" "chapters/future-work.md" > README.md
 
 # pandoc README.md > README.html
 
